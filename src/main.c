d5fcf228bd6c8bdb1d747cb3f0c5813216311a14
Pranshul Shenoy <68362349+pranshulpshenoy@users.noreply.github.com>
2024-10-27 10:05:34 +0530

    Maybe music works

--- src/main.c
+++ src/main.c
@@ -1,72 +1,77 @@
-#include "constants.h"
-#include "networking/client.c"
 #include "./main_menu.c"
 #include "./trick_or_treat.c"
+#include "constants.h"
+#include "networking/client.c"
 
+Music startMenuMusic = LoadMusicStream("../resources/audio/start_menu.wav");
+Music greenLightMusic = LoadMusicStream("../resources/audio/green_light.wav");
+Music redLightMusic = LoadMusicStream("../resources/audio/red_light.wav");
 
-int main()
-{
+int main() {
 
-    
-    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+  SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-    // Initialize Window
-    InitWindow(1920, 1080, "Overtale"); 
-    SearchAndSetResourceDir("resources");
-    Texture wabbit = LoadTexture("wabbit_alpha.png");
-    Texture textures[6] = {LoadTexture("background.png"), LoadTexture("Green.PNG"), LoadTexture("Blue.PNG"), LoadTexture("Yellow.PNG"), LoadTexture("Red.PNG"), LoadTexture("Reaper.png")};
-    
-    int SocketDescriptor = connect_to_server("10.2.132.132");
-    char buffer[1024];
-    read(SocketDescriptor, buffer, sizeof(buffer));
-    int PlayerId = buffer[0]-'0';
-    
-    int current_screen = MAIN_MENU;
-    volatile int positions[4] = {-1,-1,-1,-1};
-    volatile char state[4] = {'\0', '\0','\0','\0'};
-    positions[PlayerId] = 5;
-    pthread_t tid;
-    pthread_t tid2;
+  // Initialize Window
+  InitWindow(1920, 1080, "Overtale");
+  SearchAndSetResourceDir("resources");
+  Texture wabbit = LoadTexture("wabbit_alpha.png");
+  Texture textures[6] = {
+      LoadTexture("background.png"), LoadTexture("Green.PNG"),
+      LoadTexture("Blue.PNG"),       LoadTexture("Yellow.PNG"),
+      LoadTexture("Red.PNG"),        LoadTexture("Reaper.png")};
+  if (IsWindowReady()) {
+    StopMusicStream(greenLightMusic);
+    StopMusicStream(redLightMusic);
+    PlayMusicStream(startMenuMusic);
+  }
+  int SocketDescriptor = connect_to_server("10.2.132.132");
+  char buffer[1024];
+  read(SocketDescriptor, buffer, sizeof(buffer));
+  int PlayerId = buffer[0] - '0';
 
-    struct thread_args args = {positions, SocketDescriptor, state, PlayerId};
+  int current_screen = MAIN_MENU;
+  volatile int positions[4] = {-1, -1, -1, -1};
+  volatile char state[4] = {'\0', '\0', '\0', '\0'};
+  positions[PlayerId] = 5;
+  pthread_t tid;
+  pthread_t tid2;
 
+  struct thread_args args = {positions, SocketDescriptor, state, PlayerId};
 
-    pthread_create(&tid, NULL, receive_packets, (void*) &args);
-    pthread_create(&tid2, NULL, send_packet, (void*) &args);
-    int flag = 1;
+  pthread_create(&tid, NULL, receive_packets, (void *)&args);
+  pthread_create(&tid2, NULL, send_packet, (void *)&args);
+  int flag = 1;
 
+  while (!WindowShouldClose() && flag) {
+    BeginDrawing();
 
+    switch (current_screen) {
 
-    while (!WindowShouldClose() && flag)
-    {
-        BeginDrawing();
+    case MAIN_MENU:
+      main_menu(&current_screen);
+      break;
 
-        switch(current_screen){
+    case HOST:
+      // TODO
+      break;
 
-            case MAIN_MENU:
-                main_menu(&current_screen);
-                break;
-            
-            case HOST:
-                //TODO
-                break;
+    case CONNECT:
+      // TODO
+      break;
 
-            case CONNECT:
-                //TODO
-                break;
+    case GAME1:
+      trick_or_treat(PlayerId, &current_screen, textures, positions, state,
+                     SocketDescriptor);
+      break;
 
-            case GAME1:
-                trick_or_treat(PlayerId, &current_screen, textures, positions, state, SocketDescriptor);
-                break;
-                
-            case EXIT:
-                flag = 0;
-                break;
-        }
-        EndDrawing();
+    case EXIT:
+      flag = 0;
+      break;
     }
-    
-    UnloadTexture(wabbit);
-    CloseWindow();
-    return 0;
+    EndDrawing();
+  }
+
+  UnloadTexture(wabbit);
+  CloseWindow();
+  return 0;
 }
--- src/main_menu.c
+++ src/main_menu.c
@@ -1,55 +1,69 @@
 #include "raylib.h"
+#include "resource_dir.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "resource_dir.h"
 
-void main_menu(int* current_screen) {
+Music startMenuMusic = LoadMusicStream("../resources/audio/start_menu.wav");
+Music greenLightMusic = LoadMusicStream("../resources/audio/start_menu.wav");
+Music redLightMusic = LoadMusicStream("../resources/audio/start_menu.wav");
 
-    ClearBackground(BLACK);
-    // Button Coordinates Definitions
-    Rectangle play_button = { GetScreenWidth()/2 - 100, GetScreenHeight()/2 - 100, 200, 60 };
-    Rectangle exit_button = { GetScreenWidth()/2 - 100, GetScreenHeight()/2 + 100, 200, 60 };
+void main_menu(int *current_screen) {
 
-    DrawRectangleRec(play_button, DARKGRAY);
-    DrawRectangleRec(exit_button, DARKGRAY);
+  ClearBackground(BLACK);
+  // Button Coordinates Definitions
+  Rectangle play_button = {GetScreenWidth() / 2 - 100,
+                           GetScreenHeight() / 2 - 100, 200, 60};
+  Rectangle exit_button = {GetScreenWidth() / 2 - 100,
+                           GetScreenHeight() / 2 + 100, 200, 60};
 
-    /*
-    * DRAWING BUTTONS *
-    */
+  DrawRectangleRec(play_button, DARKGRAY);
+  DrawRectangleRec(exit_button, DARKGRAY);
 
-    // Draw Start Game Button
-    char* play_text = "Start Game";
-    Vector2 play_pos = {
-        play_button.x + play_button.width/2 - MeasureText(play_text, 30)/2,
-        play_button.y + play_button.height/2 - 15
-    };
-    DrawText(play_text, play_pos.x, play_pos.y, 30, WHITE);
+  /*
+   * DRAWING BUTTONS *
+   */
+  //   if (IsWindowReady()) {
+  //     StopMusicStream(redLightMusic);
+  //     StopMusicStream(startMenuMusic);
+  //     PlayMusicStream(greenLightMusic);
+  //   }
 
-    // Draw Exit Game Button
-    char* exit_text = "Exit Game";
-    Vector2 exit_pos = {
-        exit_button.x + exit_button.width/2 - MeasureText(exit_text, 30)/2,
-        exit_button.y + exit_button.height/2 - 15
-    };
-    DrawText(exit_text, exit_pos.x, exit_pos.y, 30, WHITE);
+  // Draw Start Game Button
+  char *play_text = "Start Game";
+  Vector2 play_pos = {play_button.x + play_button.width / 2 -
+                          MeasureText(play_text, 30) / 2,
+                      play_button.y + play_button.height / 2 - 15};
+  DrawText(play_text, play_pos.x, play_pos.y, 30, WHITE);
 
+  // Draw Exit Game Button
+  char *exit_text = "Exit Game";
+  Vector2 exit_pos = {exit_button.x + exit_button.width / 2 -
+                          MeasureText(exit_text, 30) / 2,
+                      exit_button.y + exit_button.height / 2 - 15};
+  DrawText(exit_text, exit_pos.x, exit_pos.y, 30, WHITE);
 
-    // Handle Start Game Click
-    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-        Vector2 mouse_pos = GetMousePosition();
-        printf("%d %d", mouse_pos.x, mouse_pos.y);
-        if (CheckCollisionPointRec(mouse_pos, play_button)) {
-            *current_screen = GAME1;
-        }
+  // Handle Start Game Click
+  if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+    Vector2 mouse_pos = GetMousePosition();
+    printf("%d %d", mouse_pos.x, mouse_pos.y);
+    if (CheckCollisionPointRec(mouse_pos, play_button)) {
+      StopMusicStream(redLightMusic);
+      StopMusicStream(startMenuMusic);
+      PlayMusicStream(greenLightMusic);
+      *current_screen = GAME1;
     }
+  }
 
-    // Handle Exit Game Click
-    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-        Vector2 mouse_pos = GetMousePosition();
-        if (CheckCollisionPointRec(mouse_pos, exit_button)) {
-            *current_screen = EXIT;
-        }
+  // Handle Exit Game Click
+  if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+    Vector2 mouse_pos = GetMousePosition();
+    if (CheckCollisionPointRec(mouse_pos, exit_button)) {
+      StopMusicStream(redLightMusic);
+      StopMusicStream(startMenuMusic);
+      StopMusicStream(greenLightMusic);
+      *current_screen = EXIT;
     }
-        
-    // DrawTexture(wabbit, 400, 200, WHITE);
+  }
+
+  // DrawTexture(wabbit, 400, 200, WHITE);
 }
